8ae64f509af126d46c5d3323f16fea37eb8bcaf4
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-22 19:15:57 -0300

    feat: Setup header/logo component on Event page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { gql, useQuery } from '@apollo/client'
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface ILesson {
-  id: string
-  title: string
-}
+import { Event } from './pages/Event'
 
 export function App() {
-  const { data } = useQuery<{ lessons: ILesson[] }>(GET_LESSONS_QUERY)
-  console.log(data)
-
   return (
     <>
-      <ul>
-        {data?.lessons.map(lesson => {
-          return <li key={ lesson.id }>{ lesson.title }</li>
-        })}
-      </ul>
+      <Event />
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import { Logo } from "./Logo";
+
 export function Header() {
   return (
     <>
-      Header
+      <header
+        className='w-full py-5 flex items-center justify-center bg-gray-700
+        border-b border-gray-600'>
+        <Logo />
+      </header>
     </>
   )
 }
