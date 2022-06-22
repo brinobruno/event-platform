a0cab21cdb62b2e5e3bf03dae3406fbe10933e9d
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-21 21:20:43 -0300

    task: Add ApolloProvider encapsulating App to enable useQuery hook using client created from lib as a property

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import { gql, useQuery } from '@apollo/client'
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
 export function App() {
+  const { data } = useQuery(GET_LESSONS_QUERY)
+  console.log(data)
+
   return (
     <>
     <h1 className="text-3xl">Hello world</h1>
