b1e47797a723c66f0b9d064de98494847fe398c9
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-22 20:57:05 -0300

    fix: Switch from h1 to span tag for semantic reasons

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,9 @@
 export function Logo() {
   return (
     <>
-      <h1 className='font-semibold text-3xl'>
+      <span className='font-semibold text-3xl'>
         Event<span className='text-green-400'>.io</span>
-      </h1>
+      </span>
     </>
   )
 }
