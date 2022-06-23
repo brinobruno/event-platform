0313ca482cbfb53c9f71a191896f74d1e9abaf94
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-23 08:58:44 -0300

    feat: Add video player layout from vimejs

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,10 +1,18 @@
-import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
+import { DefaultUi, Player, Youtube } from '@vime/react'
+import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from 'phosphor-react'
+
+import '@vime/core/themes/default.css'
 
 export function VideoPlayer() {
   return (
     <div className='flex-1'>
       <div className='bg-black flex justify-center'>
-        <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'></div>
+        <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'>
+          <Player>
+            <Youtube videoId='ZtMzB5CoekE' />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className='p-8 max-w-[1100px] mx-auto'>
