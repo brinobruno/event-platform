40116dec411bfcc4d321070566dfd6c321db8715
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-23 18:27:08 -0300

    task: Disable user selection for the Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo() {
   return (
     <>
-      <span className='font-semibold text-3xl'>
+      <span className='font-semibold text-3xl select-none'>
         Event<span className='text-green-400'>.io</span>
       </span>
     </>
