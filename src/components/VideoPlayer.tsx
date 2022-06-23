33717b0922e18ba1402bae3ac13e1a9331c5c875
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-23 17:49:10 -0300

    feat: Change VideoPlayer content based on slug props - cached requests as well

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,15 +1,62 @@
 import { DefaultUi, Player, Youtube } from '@vime/react'
+import { gql, useQuery } from '@apollo/client'
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from 'phosphor-react'
 
 import '@vime/core/themes/default.css'
 
-export function VideoPlayer() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string
+    videoId: string
+    description: string
+    teacher: {
+      bio: string
+      avatarURL: string
+      name: string
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string
+}
+
+export function VideoPlayer(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug
+    }
+  })
+
+  if (!data) {
+    return (
+      <div className='flex-1'>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
   return (
     <div className='flex-1'>
       <div className='bg-black flex justify-center'>
         <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'>
           <Player>
-            <Youtube videoId='ZtMzB5CoekE' />
+            <Youtube videoId={ data.lesson.videoId } />
             <DefaultUi />
           </Player>
         </div>
@@ -19,28 +66,25 @@ export function VideoPlayer() {
         <div className='flex items-start gap-16'>
           <div className='flex-1'>
             <h1 className='text-2xl font-bold'>
-              Class 01 - Open
+              { data.lesson.title }
             </h1>
             <p className='mt-4 text-gray-200 leading-relaxed'>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis 
-              consequuntur maiores laborum, sequi possimus cumque hic nemo corporis 
-              perferendis eaque neque aspernatur autem molestias iure corrupti 
-              similique quisquam tenetur quae.
+              { data.lesson.description }
             </p>
 
             <div className='flex items-center gap-4 mt-6'>
               <img
                 className='h-16 w-16 rounded-full border-2 border-blue-500'
-                src='https://github.com/brinobruno.png'
-                alt='Bruno Corrêa'
+                src={ data.lesson.teacher.avatarURL }
+                alt={ data.lesson.teacher.name }
               />
 
               <div className='leading-relaxed'>
                 <strong className='font-bold text-2xl block'>
-                  Bruno Corrêa
+                  { data.lesson.teacher.name }
                 </strong>
                 <span className='text-gray-200 text-sm block'>
-                  Junior Developer
+                  { data.lesson.teacher.bio }
                 </span>
               </div>
             </div>
