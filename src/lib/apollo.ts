14e678dfe11c307d7ab9a32f60d52cb38a8fce96
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-23 21:05:15 -0300

    build: Add Auth Token from graphcms into .env and onto Apollo headers

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,10 +1,14 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const api = {
-  contentApiURI: import.meta.env.VITE_GRAPHCMS_CONTENT_API
+  contentApiURI: import.meta.env.VITE_GRAPHCMS_CONTENT_API,
+  contentApiAuthToken: import.meta.env.VITE_GRAPHCMS_AUTH_TOKEN
 }
 
 export const client = new ApolloClient({
   uri: api.contentApiURI,
+  headers: {
+    'Authorization': `Bearer ${ api.contentApiAuthToken }`
+  },
   cache: new InMemoryCache()
 })
