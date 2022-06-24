814645e8f086428cf18aeb676a220f66e560bd4f
Bruno Corrêa <brunosantos6ft@gmail.com>
2022-06-23 21:28:16 -0300

    feat: Add disabled state dependending on mutation loading on submit button

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -18,7 +18,7 @@ export function Subscribe() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
 
-  const [createSubscriber] = useMutation(CREATE_SUBSCRIBER_MUTATION)
+  const [createSubscriber, { loading }] = useMutation(CREATE_SUBSCRIBER_MUTATION)
 
   async function handleFormSubscription(event: FormEvent) {
     event?.preventDefault()
@@ -78,7 +78,8 @@ export function Subscribe() {
 
             <button
               className='mt-4 bg-green-500 uppercase py-4 rounded font-bold
-              text-sm hover:bg-green-700 transition-colors'
+              text-sm hover:bg-green-700 transition-colors disabled:opacity-50'
+              disabled={ loading }
               type='submit'
             >
               Save my seat
